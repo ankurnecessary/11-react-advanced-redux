da021b5be80734e56ec1c6142f7b982418dbde3f
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-30 04:12:38 +0930

    Getting cart items from redux store.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,23 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import React from 'react'
+import Card from '../UI/Card'
+import classes from './Cart.module.css'
+import CartItem from './CartItem'
+import { useSelector } from 'react-redux'
 
-const Cart = (props) => {
+const Cart = () => {
+  const cartItems = useSelector(state => state.cart.items)
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.length && cartItems.map(item => <CartItem
+          key={item.title}
+          item={item}
+        />)}
+
       </ul>
     </Card>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
