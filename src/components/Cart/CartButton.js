ac608f5e98baf1a9fdc96da3a20548062088cc48
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-30 04:15:11 +0930

    Showing total no. of products on cart button.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,22 @@
+import React from 'react'
+import classes from './CartButton.module.css'
 import { useDispatch, useSelector } from 'react-redux'
+import { cartActions } from '../../store'
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const quantity = useSelector(state => state.cart.items.length)
+
   function toggleCart () {
     dispatch(cartActions.toggleCart())
   }
+
   return (
     <button onClick={toggleCart} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
-  );
-};
+  )
+}
 
-export default CartButton;
+export default CartButton
